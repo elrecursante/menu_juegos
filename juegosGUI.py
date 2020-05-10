99347645b4e5262691925dd009670eb9875df827
elrecursante <63516124+elrecursante@users.noreply.github.com>
2020-05-10 15:37:15 -0300

    Menu para uego
    Es un menu  que permite elegir entre tres juego. 
    Guarda el nombre de usuario y al juego que jugo

---
+++ juegosGUI.py
@@ -0,0 +1,69 @@
+import hangman
+import reversegam
+import tictactoeModificado
+import PySimpleGUI as gui
+import time
+import json
+
+
+
+def guardar_usuario(usr):
+
+	try:
+		with open ("historialUsuarios.json",'r+', encoding='UTF-8') as a:
+			usuarios = a.read()
+			json.dump(usr, a, indent= 3 )
+
+	except FileNotFoundError:
+		with open("historialUsuarios.json", 'w', encoding='UTF-8') as a:
+			json.dump(usr,a ,indent=3)
+
+def main(args):
+	
+	sigo_jugando = True
+	while sigo_jugando:
+		
+
+
+		layout = [[gui.Text('Menu de Juegos')],
+				  [gui.Text('usuario:'), gui.InputText(size=(10, 50), key='usuario')],
+				  [gui.Button(button_text='Ahoracado', key='ahorcado')],
+				  [gui.Button(button_text='TA-TE-Ti', key='tateti')],
+					[gui.Button(button_text='otello', key='otello')],
+					[gui.Button(button_text='Salir', key='salir')]
+		]
+
+		window = gui.Window('Juegos', layout)
+		window.Finalize()
+		event, value = window.read()
+
+		if value  != None :
+			usr = {value['usuario']:[event, time.time()]}
+
+		if event == 'ahorcado':
+			window.close()
+
+			guardar_usuario(usr)
+			hangman.main()
+			sigo_jugando = False
+		elif event == 'tateti':
+			window.close()
+			guardar_usuario(usr)
+			tictactoeModificado.main()
+
+			sigo_jugando = False
+
+		elif event == 'otello':
+			window.close()
+			guardar_usuario(usr)
+			reversegam.main()
+			sigo_jugando = False
+
+		elif event == 'salir' :
+			sigo_jugando = False
+			window.close()
+	window.close()
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
